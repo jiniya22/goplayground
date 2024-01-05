aff4e77956167200622c68d87ea57d32a2cdccb2
jini <jini@jiniworld.me>
2024-01-05 10:01:33 +0900

    :sparkles: 구조체

--- ch13/ex13.2/ex13.2.go
+++ ch13/ex13.2/ex13.2.go
@@ -7,13 +7,20 @@ import (
 
 type Student struct {
 	Age   int32
+	F1    int8
+	F2    int8
 	Score float64
 }
 
 func main() {
 
 	u1 := Student{Age: 32, Score: 9.5}
-	fmt.Println(unsafe.Sizeof(u1), &u1.Age, &u1.Score)
-	fmt.Printf("%d, %d, %d\n", unsafe.Sizeof(u1), &u1.Age, &u1.Score)
+	fmt.Println(unsafe.Sizeof(u1), &u1.Age, &u1.F1, &u1.F2, &u1.Score)
+
+	fmt.Printf("%d, %d, %d, %d, %d\n", unsafe.Sizeof(u1), &u1.Age, &u1.F1, &u1.F2, &u1.Score)
+
+	//u1 := Student{Age: 32, Score: 9.5}
+	//fmt.Println(unsafe.Sizeof(u1), &u1.Age, &u1.Score)
+	//fmt.Printf("%d, %d, %d\n", unsafe.Sizeof(u1), &u1.Age, &u1.Score)
 
 }
